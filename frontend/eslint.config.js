1ccf4cc23da89e4fc6b9a5565a023593995942ff
kallekekale <kalle.kekale@icloud.com>
2025-10-27 18:21:08 +0200

    Setup frontend

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,25 +1,23 @@
-import js from "@eslint/js";
-import prettierConfig from "eslint-config-prettier";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default [
-  js.configs.recommended,
-  prettierConfig,
+export default defineConfig([
+  globalIgnores(['dist']),
   {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: {
-        console: "readonly",
-        process: "readonly",
-        __dirname: "readonly",
-        __filename: "readonly",
-        module: "readonly",
-        require: "readonly",
-      },
-    },
-    rules: {
-      // Add your custom rules here
+      ecmaVersion: 2020,
+      globals: globals.browser,
     },
   },
-];
+])
