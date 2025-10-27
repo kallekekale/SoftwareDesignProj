82ecbfb25a294425066d2fb21f2564e11070dcdf
Juho Kangas <156090779+juhokan@users.noreply.github.com>
2025-10-27 11:13:00 +0200

    Create backend (#3)

---
+++ backend/src/test/java/fi/tuni/softwaredesign/SoftwaredesignApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.tuni.softwaredesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwaredesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
