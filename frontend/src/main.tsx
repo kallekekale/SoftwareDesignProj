47e7bf394f12977fab10f759a67d24c59ae534d4
kallekekale <kalle.kekale@icloud.com>
2025-10-28 17:04:10 +0200

    prettier

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,5 +12,5 @@ createRoot(document.getElementById("root")!).render(
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </StrictMode>
+  </StrictMode>,
 );
