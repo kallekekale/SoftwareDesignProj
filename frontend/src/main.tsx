e6f261d561ab1276177710e10949d64e6a66740f
kallekekale <kalle.kekale@icloud.com>
2025-10-28 17:03:10 +0200

    move queryClient to its own file under new lib directory

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./App.tsx";
-
-// Create a client
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+import { queryClient } from "./lib/queryClient.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -21,5 +12,5 @@ createRoot(document.getElementById("root")!).render(
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  </StrictMode>,
+  </StrictMode>
 );
