79c2367c4bbf083eda72e4f964de666083e9bed8
Juho Kangas <juho.a.kangas@tuni.fi>
2025-10-29 13:35:38 +0200

    Remove unused code

--- backend/src/main/java/fi/tuni/softwaredesign/http/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * HTTP request utility classes for making external API calls. Provides a clean abstraction layer
- * over Spring's RestTemplate for HTTP operations.
- */
-package fi.tuni.softwaredesign.http;
