1bcec0e4296474708fe9cb05b9a3d85d9104747a
Aleksanteri <alexanteri@gmail.com>
2025-12-01 23:22:41 +0200

    add dto

---
+++ backend/src/main/java/fi/tuni/softwaredesign/search/BreweryWithRestaurantsDto.java
@@ -0,0 +1,9 @@
+package fi.tuni.softwaredesign.search;
+
+import fi.tuni.softwaredesign.shared.domain.dto.response.OpenBreweryDbDistanceResponseDto;
+import fi.tuni.softwaredesign.shared.domain.dto.response.YelpBusinessDistanceResponseDto;
+import java.util.List;
+
+public record BreweryWithRestaurantsDto(
+    OpenBreweryDbDistanceResponseDto brewery,
+    List<YelpBusinessDistanceResponseDto> restaurants) {}
